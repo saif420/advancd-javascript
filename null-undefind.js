0fc17d756ea9ead8a47d344d5210c4a341369cdf
saif420 <saifmahmud42069@gmail.com>
2020-08-07 08:39:26 +0600

    null vs undefined

---
+++ null-undefind.js
@@ -0,0 +1,20 @@
+// undefined 
+// let name;
+// funcation  return nah lekla 
+// funcation  namer value set nah korla
+let name;
+console.log(name);
+
+// function age (aga1, aga2){
+// }
+// let total = age (10, 20);
+// console.log(total); 
+
+// function age (aga1, aga2){
+//     console.log(aga1 + aga2);
+// }
+// const total = age (10);
+// console.log(total); 
+
+let age = [2,5,6];
+console.log(age[11]);
