a70b30b70ed6d9f43417cdc2b8a404b8bccd51f3
saif420 <saifmahmud42069@gmail.com>
2020-08-07 15:52:19 +0600

    return and continue

---
+++ array-splice.js
@@ -0,0 +1,8 @@
+const num = [1, 2, 3 ,4 ,5 ,6 ,7 ,8];
+const part = num.splice(2,6, 34, 44, 54,);
+
+const togther = num.join("ami");
+
+console.log(togther);
+console.log(num);
+
---
+++ break-continu.js
@@ -0,0 +1,7 @@
+const num = [1, 2, 3 ,4 ,5 ,6 ,7 ,8];
+for (let i=0; i < num.length; i++) {
+    if (num[i]>6) {
+        break;
+    }
+    console.log(num[i]);
+}
