7b08d77d7a8d1369c8b0f4c66db36f2d1abca1ea
saif420 <saifmahmud42069@gmail.com>
2020-08-07 10:56:40 +0600

    map, filter, dind

--- object-proparty.js
+++ object-proparty.js
@@ -5,6 +5,7 @@ const students = [
     {id:44, name:'raju'},
 ];
 const name = students.map(element => element.id);
-console.log(name);
+const bigger = students.find(element => element.id > 40);
+console.log(bigger);
 
 
