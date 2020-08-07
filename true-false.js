d1aab06e789a01863c216a58c76536de6503661d
saif420 <saifmahmud42069@gmail.com>
2020-08-07 08:14:17 +0600

    false vs true

---
+++ true-false.js
@@ -0,0 +1,20 @@
+// false value
+// 0
+// ''
+// null
+// NaN
+// undefined
+// false
+
+
+
+// true value
+// 'false', '0',{},[]
+
+const name = {};
+if(name){
+    console.log(true);
+}
+else{
+    console.log(false);
+}
